4c25d0e9c014aba0bdd6cd01d6d02856e14fae8b
Gank Lanceman <ganklanceman@Ganks-MacBook-Pro.local>
2018-04-08 12:48:37 -0500

    Badge styling improvements

--- src/components/badge-item.js
+++ src/components/badge-item.js
@@ -3,8 +3,10 @@ import React from "react";
 const BadgeItem = ({ name, imageUrl }) => {
   return (
     <div className="badge">
-      <img src={imageUrl} alt="" className="badge-image" />
-      {name}
+      <img src={imageUrl} alt={name} className="badge-image" />
+      <div title={name}>
+        {name}
+      </div>
     </div>
   )
 }
